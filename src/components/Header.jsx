c0fefddbb1493011c51def63a74e10fc95f3079e
afnan <afnanballoor44@gmail.com>
2023-12-10 21:25:40 +0530

    1 insalled react-toastify module  2.updated app.jsx   3. updated LoginScreen.jsx  4. updated header.jsx  5. updated usersApiSlice  6. updated LoginScreen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container,NavDropdown, Badge } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt} from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice'
+import { useNavigate } from 'react-router-dom';
 const Header = () => {
+    const { userInfo } = useSelector((state)=> state.auth)
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall.unwrap();
+            dispatch(logout());
+            navigate('/')
+        } catch (err) {
+            console.log(err);
+        }
+    }
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -13,7 +32,22 @@ const Header = () => {
                     <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ms-auto'>
-                            <LinkContainer to='/login'>
+                            { userInfo ? (
+                                <>
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>
+                                            Profile
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                                </>
+                            ) : (
+                                <>
+                                <LinkContainer to='/login'>
                              <Nav.Link>
                                 <FaSignInAlt/> Sign In
                              </Nav.Link>
@@ -23,6 +57,9 @@ const Header = () => {
                                 <FaSignInAlt/> Sign Up
                              </Nav.Link>
                             </LinkContainer>
+                                </>
+                            )}
+                            
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
